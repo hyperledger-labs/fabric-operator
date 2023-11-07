13376c80fad2475b8541896bee754acff6f1ee48
Shoaeb Jindani <jindani.shoaeb@ibm.com>
2023-11-07 21:27:45 +0530

    Reverting Use tags default to true
    Signed-off-by: Shoaeb Jindani <jindani.shoaeb@ibm.com>

--- api/v1beta1/ibpconsole.go
+++ api/v1beta1/ibpconsole.go
@@ -36,8 +36,8 @@ func (s *IBPConsole) GetMSPID() string {
 }
 
 func (s *IBPConsole) UseTags() bool {
-	useTags := true
-	if s.Spec.UseTags != nil {
+	useTags := false
+	if s.Spec.UseTags != nil && *(s.Spec.UseTags) {
 		useTags = *s.Spec.UseTags
 	}
 	return useTags
